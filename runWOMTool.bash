b2f520603237205c78fbd680ce2b3c1e09ec8f28
Matt Johnson <matthew.j@wustl.edu>
2022-03-15 11:57:59 -0500

    Scripts to begin testing on compute1

--- runWOMTool.bash
+++ runWOMTool.bash
@@ -4,13 +4,15 @@ export LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchaga
 /scratch1/fs1/cruchagac:/scratch1/fs1/cruchagac"
 JOB_GROUP="/${USER}/compute-cruchagac"
 bgadd -L 10 ${JOB_GROUP}
+MODE="$1"
+WDL="$2"
 export JAVA_OPTS="-Dconfig.file=/scratch1/fs1/cruchagac/Workflows/testMJ.conf"
-WDL="$1"
 #LSF_DOCKER_ENV_FILE="/scratch1/fs1/cruchagac/${USER}/c1in/envs/${FULLSMID}.env" \
 bsub -g ${JOB_GROUP} \
 -J ${USER}/cromwell/${WDL} \
 -n 1 \
--R 'select[port8644=1]' \
+-R 'rusage[mem=8000]' \
+-M 8000
 -G compute-cruchagac \
 -q general-interactive \
--a 'docker(broadinstitute/cromwell:77)' java -Dconfig.file=/scratch1/fs1/cruchagac/Workflows/testMJ.conf -jar cromwell-${VERSION}.jar run ${WDL}
+-a 'docker(broadinstitute/womtool:77)' ${MODE} ${WDL}
