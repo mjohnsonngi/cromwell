32758b6bcbd46470e31d5de54d6987851877bc62
Matt Johnson <matthew.j@wustl.edu>
2022-03-16 10:53:39 -0500

    server implementation

--- runCromwell.bash
+++ runCromwell.bash
@@ -1,16 +1,17 @@
 #!/bin/bash
-export VERSION=77
-export LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
-/scratch1/fs1/cruchagac:/scratch1/fs1/cruchagac"
 JOB_GROUP="/${USER}/compute-cruchagac"
 bgadd -L 10 ${JOB_GROUP}
-export JAVA_OPTS="-Dconfig.file=/scratch1/fs1/cruchagac/Workflows/testMJ.conf"
+#JAVA_OPTS="-Dconfig.file=/scratch1/fs1/cruchagac/Workflows/testMJ.conf"
 WDL="$1"
-#LSF_DOCKER_ENV_FILE="/scratch1/fs1/cruchagac/${USER}/c1in/envs/${FULLSMID}.env" \
+LSF_DOCKER_PRESERVE_ENVIRONMENT=false \
+LSF_DOCKER_ENTRYPOINT=/bin/bash \
+LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
+/scratch1/fs1/cruchagac:/scratch1/fs1/cruchagac" \
 bsub -g ${JOB_GROUP} \
 -J ${USER}/cromwell/${WDL} \
+-Is \
 -n 1 \
 -R 'select[port8644=1]' \
 -G compute-cruchagac \
 -q general-interactive \
--a 'docker(broadinstitute/cromwell:77)' java -Dconfig.file=/scratch1/fs1/cruchagac/Workflows/testMJ.conf -jar cromwell-${VERSION}.jar run ${WDL}
+-a 'docker(broadinstitute/cromwell:77)' java ${JAVA_OPTS} -jar /app/cromwell.jar server
