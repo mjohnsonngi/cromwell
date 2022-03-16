f3af9483b348ea7f2b3ecdf762b680052e48fe0e
Matt Johnson c1 <matthew.j@wustl.edu>
2022-03-16 16:29:27 -0500

    updated running config

--- cromwellRun.bash
+++ cromwellRun.bash
@@ -4,7 +4,6 @@ bgadd -L 10 ${JOB_GROUP}
 JAVA_OPTS="-Dconfig.file=./testMJ.conf"
 WDL="$1"
 INPUTS="$2"
-LSF_DOCKER_PRESERVE_ENVIRONMENT=false \
 LSF_DOCKER_PORTS='8644:8644' \
 LSF_DOCKER_ENTRYPOINT=/bin/bash \
 LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
@@ -14,5 +13,6 @@ bsub -g ${JOB_GROUP} \
 -n 1 \
 -R 'select[port8644=1]' \
 -G compute-cruchagac \
--q general \
--a 'docker(broadinstitute/cromwell:77)' java ${JAVA_OPTS} -jar /app/cromwell.jar run $@
+-q general-interactive \
+-Is \
+-a 'docker(broadinstitute/cromwell:77)' /opt/java/openjdk/bin/java $JAVA_OPTS -jar /app/cromwell.jar run $@
