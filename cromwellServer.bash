ea5240dc2cd288d375551274a6257949953e153d
Matt Johnson <matthew.j@wustl.edu>
2022-03-16 12:10:42 -0500

    split script

--- cromwellServer.bash
+++ cromwellServer.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 JOB_GROUP="/${USER}/compute-cruchagac"
 bgadd -L 10 ${JOB_GROUP}
-#JAVA_OPTS="-Dconfig.file=/scratch1/fs1/cruchagac/Workflows/testMJ.conf"
+JAVA_OPTS="-Dconfig.file=./testMJ.conf"
 WDL="$1"
 LSF_DOCKER_PRESERVE_ENVIRONMENT=false \
-LSF_DOCKER_PORTS='8644:8000' \
+LSF_DOCKER_PORTS='8644:8644' \
 LSF_DOCKER_ENTRYPOINT=/bin/bash \
 LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
 /scratch1/fs1/cruchagac:/scratch1/fs1/cruchagac" \
